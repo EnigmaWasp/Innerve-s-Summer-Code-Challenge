4f34b7293734a9b20fdce5a5b29d4128b609eb3d
Dagny Laib <dagny.danneskjold@gmail.com>
2017-08-08 12:42:46 +0300

    Add files via upload

---
+++ Patrol.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	int t;
+	double u, v, x;
+	cin >> t;
+	for(int i = 0; i < t; i++)
+	{
+		cin >> u >> v >> x;
+		printf("%.10lf\n", x/(u+v));
+	}
+	return 0;
+} 
